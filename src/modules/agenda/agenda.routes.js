1714d970f38a5c9524e49148bed85ee2ec58fa56
Camilo-tecnico <jhoancamira@gmail.com>
2026-04-29 16:01:50 -0500

    Update agenda.routes.js

--- src/modules/agenda/agenda.routes.js
+++ src/modules/agenda/agenda.routes.js
@@ -1,17 +1,19 @@
 import express from 'express';
-import { verificarToken } from '../../middlewares/auth.middleware.js';
-import * as agendaCtrl from './agenda.controller.js';
-import * as tareaCtrl  from '../tareas/tareas.controller.js';
+import { verificarToken } from '../middlewares/auth.middleware.js';
+import * as agendaCtrl from '../controllers/agenda.controller.js';
+import * as tareaCtrl from '../controllers/tareas.controller.js';
 
 const router = express.Router();
 
 router.use(verificarToken);
 
-router.get('/dia',            agendaCtrl.obtenerAgendaDia);
-router.get('/estadisticas',   agendaCtrl.obtenerEstadisticas);
+// 📅 agenda
+router.get('/dia', agendaCtrl.obtenerAgendaDia);
+router.get('/estadisticas', agendaCtrl.obtenerEstadisticas);
 
-router.get('/categorias',        tareaCtrl.obtenerCategorias);
-router.post('/categorias',       tareaCtrl.crearCategoria);
+// 🏷️ categorias
+router.get('/categorias', tareaCtrl.obtenerCategorias);
+router.post('/categorias', tareaCtrl.crearCategoria);
 router.delete('/categorias/:id', tareaCtrl.eliminarCategoria);
 
 export { router as agendaRouter };
